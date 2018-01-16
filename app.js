2955999a90bff92e0f505b8d119373879c0a2557
creekhu <creekthu@gmail.com>
2018-01-16 13:31:17 -0800

    set up routes

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
 
+const app = express();
+
+const port = process.env.PORT || 5000;
+const nav = [
+	{
+	    Link: '/Books',
+	    Text: 'Book'
+    }, 
+    {
+	    Link: '/Authors',
+	    Text: 'Author'
+    }
+];
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
 
+app.set('view engine', 'ejs');
 
-//route to homepage
+app.use('/Books', bookRouter);
+
 app.get('/', (req, res) => {
-	res.send('Hello World');
+    res.render('index', {
+        title: 'Hello from render',
+        nav,
+    });
 });
 
 app.get('/books', (req, res) => {
-	res.send('Books');
+    res.send('Hello Books');
 });
 
 app.listen(port, err => {
-	console.log('Running server on ' + port);
+    console.log('running server on port ' + port);
 });
-
