6046d85a5af10490d5a494a2d8bbcaf783f9f51b
creekhu <creekthu@gmail.com>
2018-01-15 16:08:19 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+
+//route to homepage
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+app.get('/books', (req, res) => {
+	res.send('Books');
+});
+
+app.listen(port, err => {
+	console.log('Running server on ' + port);
+});
+
